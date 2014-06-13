e722820c7065d19ae174b11d3c8753ef9149118a
Tom Nullet <thomasnullet@gmail.com>
2014-06-12 19:59:47 -0500

    models/migrations covered and passing

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -9,4 +9,10 @@ describe Event do
   it { should belong_to(:requester).class_name('User')}
   it { should belong_to(:responder).class_name('User')}
 
+  it { should have_db_column(:requester_id) }
+  it { should have_db_column(:responder_id) }
+  it { should have_db_column(:latitude) }
+  it { should have_db_column(:longitude) }
+  it { should have_db_column(:active) }
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,14 @@ describe User do
   it { should have_many(:requests).class_name('Event').with_foreign_key('requester_id') }
   it { should have_many(:responses).class_name('Event').with_foreign_key('responder_id') }
 
+  it { should have_db_column(:name) }
+  it { should have_db_column(:email) }
+  it { should have_db_column(:password) }
+  it { should have_db_column(:phone) }
+  it { should have_db_column(:hb_latitude) }
+  it { should have_db_column(:hb_longitude) }
+  it { should have_db_column(:hb_radius) }
+  it { should have_db_column(:home_base) }
+  it { should have_db_column(:karma_count) }
+
 end
