af292f214dcfe580f29c3f69b42e5ce7c2abcdf8
Robb Tvorik <ieatkimchi@gmail.com>
2014-06-15 00:40:46 -0500

    message

--- db/migrate/user_migrate.rb
+++ db/migrate/user_migrate.rb
@@ -1,17 +1,17 @@
-class CreateUsers < ActiveRecord::Migration
-  def change
-    create_table :users do |t|
-      t.string  :name
-      t.string  :email
-      t.string  :password
-      t.string  :phone
-      t.integer :karma_count
-      t.string  :home_base
-      t.float   :hb_latitude
-      t.float   :hb_longitude
-      t.float   :hb_radius
+# class CreateUsers < ActiveRecord::Migration
+#   def change
+#     create_table :users do |t|
+#       t.string  :name
+#       t.string  :email
+#       t.string  :password
+#       t.string  :phone
+#       t.integer :karma_count
+#       t.string  :home_base
+#       t.float   :hb_latitude
+#       t.float   :hb_longitude
+#       t.float   :hb_radius
 
-      t.timestamps
-    end
-  end
-end
+#       t.timestamps
+#     end
+#   end
+# end
