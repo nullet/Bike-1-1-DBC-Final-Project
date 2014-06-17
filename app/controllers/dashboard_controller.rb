a622f40de51c6a6ed5c4fa0c66fac7bf0732dd30
Robb Tvorik <ieatkimchi@gmail.com>
2014-06-17 13:46:54 -0500

    fixed old file

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,7 +4,7 @@ class DashboardController < ApplicationController
 	def index
 		@users = User.all
 		@user = current_user
-		@events = Event.where(active: true)
+		# @events = Event.where(active: true)
 		@events = current_user.nearby_requests
 		# @events = Event.all
 	end
