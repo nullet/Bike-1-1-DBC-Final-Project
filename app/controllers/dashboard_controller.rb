851183ed87700fd74b9768b71344c34cf3da8d8b
Jared M Daugherty & WarpDrive McNate <pair+jmdaugherty.natekandler@devbootcamp.com>
2014-06-19 10:05:58 -0500

    working on mobile login logic

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,9 @@
 class DashboardController < ApplicationController
 	before_action :authenticate_user!
-	
+
 	def index
 		@users = User.all
-		@events = current_user.nearby_requests
+		@events = Event.where(active: true)
 	end
 
   def karma
